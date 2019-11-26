c47df4b17211dcdcd95b439c96ab9fcbd91e4df5
Ahmet Durmic <ahmetdurmic97@outlook.com>
2019-11-26 22:54:41 +0100

    Display of customers and movies

---
+++ MovieApp/MovieApp/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieApp.Models;
+namespace MovieApp.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie movie { get; set; }
+        public List<Customer> customerList { get; set; }
+
+        public void setMovie()
+        {
+            movie.Name = "Brzi i Zestoki";
+        }
+
+        public void setCustomers()
+        {
+            customerList.Add(new Customer() {Id=1, Name = "Edo" });
+            customerList.Add(new Customer() {Id=2, Name = "Ado" });
+        }
+
+        public string getName(int i)
+        {
+            return customerList[i].Name;
+        }
+
+        public int getId(int i)
+        {
+            return customerList[i].Id;
+        }
+
+    }
+}
