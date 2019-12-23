f835a98d822b292ece3022e32a0168af1a2d1d85
Ahmet Durmic <ahmetdurmic97@outlook.com>
2019-12-23 22:40:37 +0100

    Added movieDto

---
+++ MovieApp/MovieApp/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Movie name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Date of release")]
+        public string ReleaseDate { get; set; }
+
+        public string DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public short StockNumber { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+    }
+}
