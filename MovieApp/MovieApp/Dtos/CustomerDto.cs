15a5a6f8a6f8a264fb49b4a015803a2c43e7fe49
Ahmet Durmic <ahmetdurmic97@outlook.com>
2019-12-23 22:40:16 +0100

    Added CustomerDto

---
+++ MovieApp/MovieApp/Dtos/CustomerDto.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required()]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required()]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        [StringLength(15)]
+        public string BirthDate { get; set; }
+
+        public int MembershipTypeId { get; set; }
+        
+    }
+}
